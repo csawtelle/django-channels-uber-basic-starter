829261567c7ca45582223ed83fee19fade8b994c
csawtelle <csawtelle@live.com>
2017-11-15 16:23:00 -0800

    Fixups and html changes

--- chat/views.py
+++ chat/views.py
@@ -8,12 +8,20 @@ from .models import Message, Conversation, Contacts
 @login_required
 def ir(request):
   user = request.user
+  conversations = Conversation.objects.filter(participants__contains=[user.username])
+  chatdata = []
+  for conversation in conversations:
+    chatdata.append({
+      "participants": (",".join(str(x) for x in conversation.participants)),
+      "messages": Message.objects.filter(conversation=conversation)
+    })
+
   return render(
     request, 
     "ir/ir.html", 
     {
       'username': user,
-      'recipients': ['user1', 'user2', 'user3'],
+      'chatdata': chatdata
     }
   )
 
@@ -21,8 +29,13 @@ def ir(request):
 def sendMessage(request):
   user = request.user
   if(request.method == "POST"):
-    receiver = request.POST.getlist('consumer[]')
-    receiver.append(user.username)
+    receiver = request.POST['consumer']
+    print(receiver)
+    receiver = receiver.split(',')
+    if request.user.username not in receiver:
+      receiver.append(user.username)
+    receiver = list(set(receiver))
+    print(receiver)
     message = request.POST['message']
     conversation, created = Conversation.objects.get_or_create(participants=receiver)
     Message.objects.create(creator=user, content=message, conversation=conversation)
--- imageRecognition/consumers.py
+++ imageRecognition/consumers.py
@@ -1,14 +1,13 @@
 from channels import Group
 
 def ws_connect(message):
-    Group("sensor").add(message.reply_channel)
-    message.reply_channel.send({
-      "text": "Connected",
-    })
+  Group("sensor").add(message.reply_channel)
+  Group("sensor").send({"accept": True})
+  print("Websocket user connected.")
 
 def ws_message(message):
-    print("Received!!" + message['text'])
+  print("Received!!" + message['text'])
 
 def ws_disconnect(message):
-  print("Someone left us...")
+  print("Websocket user disconnected.")
   Group("sensor").discard(message.reply_channel)
